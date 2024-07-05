90d95d07417ac830f46fe9da84947850a4ad7c4f
GabrielDev <gabrys.wisniewski@op.pl>
2024-07-05 15:52:19 +0200

    update: model and dataset

--- server/main.py
+++ server/main.py
@@ -1,75 +1,82 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+from tensorflow.keras import  layers, models
+import random
 import os
-from keras import layers, models
 
-class_names = ["moon", "star",  "asteroid", "meteroite", "planet", "nebula", "galaxy"]
-class_indices = {class_name: i for i, class_name in enumerate(class_names)}
-
-image_path = os.path.join(os.path.dirname(__file__), './datasets')
-image_files = os.listdir(image_path)
-
+main_dataset_path = os.path.join(os.path.dirname(__file__), 'datasets')
+training_images = []
+testing_images = []
+trainig_labels = []
+testing_labels = []
 images = []
 labels = []
+subfolders = [f.name for f in os.scandir(main_dataset_path) if f.is_dir()]
 
-for filename in os.listdir(image_path):
-    if filename.endswith('.jpg'):
-        img = cv.imread(os.path.join(image_path, filename))
-        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        img = cv.resize(img, (32, 32))
-        images.append(img)
-        label = os.path.splitext(filename)[0]
-        labels.append(label)
+class_names = ['galaxy', 'moon', 'nebula', 'planet', 'star']
 
-images = np.array(images)
-labels = np.array(labels)
 
-# Mapowanie nazw plików na indeksy klas
-label_indices = np.array([class_indices[label] for label in labels])
+for subfolder in subfolders:
+    #labels.append(subfolder)
+    print(subfolder)
+    label_index = class_names.index(subfolder) 
+    subfolder_path = os.path.join(main_dataset_path, subfolder)
 
-#plt.figure(figsize=(10, 10))
-for i in range(7):
-    plt.subplot(4, 4, i + 1)
+    for root, dirs, files in os.walk(subfolder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            img = cv.imread(file_path)
+            img = cv.resize(img, (32,32))
+            images.append(img)
+            labels.append(label_index)
+            
+
+
+random.shuffle(images)
+split_index = len(images) // 2
+
+
+print("training:")
+training_images = np.array(images[:split_index])
+trainig_labels = np.array(labels[:split_index])
+print(training_images)
+print("----------------------")
+print("Testing")
+testing_images = np.array(images[split_index:])
+testing_labels  = np.array(labels[split_index:])
+print(testing_images)
+
+for i in range(30):
+    plt.subplot(10,3, i+1)
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[label_indices[i]])
+    plt.imshow(training_images[i])
+    plt.xlabel(class_names[trainig_labels[i]])
 plt.show()
 
-
-images = images[:7]
-label_indices = label_indices[:7]  # Używamy indeksów klas
-
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
 model.add(layers.Flatten())
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.BatchNormalization())  # Warstwa normalizacji wsadowej
-model.add(layers.Dropout(0.5))  # Warstwa Dropout do regularyzacji
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(7, activation='softmax'))
+model.add(layers.Dense(64, activation="relu"))
+model.add(layers.Dense(10, activation="softmax"))
 
+model.compile(optimizer='adam', loss="sparse_categorical_crossentropy",metrics=['accuracy'])
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, trainig_labels, epochs=10, validation_data=(testing_images, testing_labels))
 
-
-
-model.fit(images, label_indices, epochs=100, validation_data=(images, label_indices))
-
-img_path = os.path.join(os.path.dirname(__file__), "planet.jpg")
+img_path = os.path.join(os.path.dirname(__file__), 'galaxy.jpg')
 img = cv.imread(img_path)
-img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-img = cv.resize(img, (32, 32))
-
-plt.imshow(img, cmap=plt.cm.binary)
+img = cv.resize(img, (32,32))
+img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
 
 prediction = model.predict(np.array([img]) / 255)
 index = np.argmax(prediction)
-predicted_class_name = class_names[index]
-print(f"prediction is {predicted_class_name}")
+print(f"prediction is {class_names[index]}")
+plt.imshow(img)
+plt.show()
