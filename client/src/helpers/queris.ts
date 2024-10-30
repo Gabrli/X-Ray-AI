97e0357f0e3c638e4a3c29a71b30185ccc8d9198
GabrielDev <gabrys.wisniewski@op.pl>
2024-10-30 17:21:47 +0100

    update: queries

--- client/src/helpers/queris.ts
+++ client/src/helpers/queris.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const ANALYSIS_POST_QUERY = async (formData: FormData) => {
-  return await axios.post(`https://x-ray-iq-4now.vercel.app/prompt`, formData);
+  return await axios.post(`https://x-ray-iq.onrender.com/prompt`, formData);
 };
